dbc65bf42449db797348d21479332e60c30e04d4
Krabbyspatties <orcaadmirer2@gmail.com>
2025-03-24 21:56:15 +0800

    first commit

---
+++ server/database/migrations/2025_03_24_132427_create_tbl_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->String('first_name', 55);
+            $table->String('middle_name', 55)->nullable();
+            $table->String('last_name', 55);
+            $table->String('suffix_name', 55)->nullable();
+            $table->integer('age');
+            $table->unsignedBigInteger('gender_id');
+            $table->String('address', 255);
+            $table->String('contact_number', 55);
+            $table->String('email', 55)->unique();
+            $table->String('password', 255);
+            $table->timestamps();
+
+            $table->foreign('gender_id')
+                ->references('gender_id')
+                ->on('tbl_genders')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_users');
+    }
+};
